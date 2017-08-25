f891c854a2b439b2a931cff337b43d81b09171a9
陈增辉 <conlay@qq.com>
2017-08-25 20:16:43 +0800

    init

---
+++ src/main/java/com/logibeat/cloud/common/model/BaseEntity.java
@@ -0,0 +1,36 @@
+package com.logibeat.cloud.common.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public abstract class BaseEntity implements Serializable{
+    private static final long serialVersionUID = -6965751606920939495L;
+
+    private Long id;
+    private Date updateAt;
+    private Date createAt;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getUpdateAt() {
+        return updateAt;
+    }
+
+    public void setUpdateAt(Date updateAt) {
+        this.updateAt = updateAt;
+    }
+
+    public Date getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(Date createAt) {
+        this.createAt = createAt;
+    }
+}
---
+++ src/main/java/com/logibeat/cloud/common/model/EntityCriteria.java
@@ -0,0 +1,44 @@
+package com.logibeat.cloud.common.model;
+
+import com.google.common.collect.Range;
+
+import java.util.Date;
+
+public class EntityCriteria {
+    private Date updateAt;
+    private Date createAt;
+    private Range<Date> updateRange;
+    private Range<Date> createRange;
+
+    public Date getUpdateAt() {
+        return updateAt;
+    }
+
+    public void setUpdateAt(Date updateAt) {
+        this.updateAt = updateAt;
+    }
+
+    public Date getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(Date createAt) {
+        this.createAt = createAt;
+    }
+
+    public Range<Date> getUpdateRange() {
+        return updateRange;
+    }
+
+    public void setUpdateRange(Range<Date> updateRange) {
+        this.updateRange = updateRange;
+    }
+
+    public Range<Date> getCreateRange() {
+        return createRange;
+    }
+
+    public void setCreateRange(Range<Date> createRange) {
+        this.createRange = createRange;
+    }
+}
