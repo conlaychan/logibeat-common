79ee403b9f01518a4faecb317a56fb4902a7e575
陈增辉 <conlay@qq.com>
2017-11-15 14:19:59 +0800

    @Data

--- src/main/java/com/logibeat/cloud/common/model/BaseEntity.java
+++ src/main/java/com/logibeat/cloud/common/model/BaseEntity.java
@@ -1,8 +1,11 @@
 package com.logibeat.cloud.common.model;
 
+import lombok.Data;
+
 import java.io.Serializable;
 import java.util.Date;
 
+@Data
 public abstract class BaseEntity implements Serializable {
     private static final long serialVersionUID = -6965751606920939495L;
 
@@ -10,27 +13,4 @@ public abstract class BaseEntity implements Serializable {
     private Date updateAt;
     private Date createAt;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Date getUpdateAt() {
-        return updateAt;
-    }
-
-    public void setUpdateAt(Date updateAt) {
-        this.updateAt = updateAt;
-    }
-
-    public Date getCreateAt() {
-        return createAt;
-    }
-
-    public void setCreateAt(Date createAt) {
-        this.createAt = createAt;
-    }
 }
--- src/main/java/com/logibeat/cloud/common/model/EntityCriteria.java
+++ src/main/java/com/logibeat/cloud/common/model/EntityCriteria.java
@@ -1,8 +1,13 @@
 package com.logibeat.cloud.common.model;
 
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.Setter;
+
 import java.io.Serializable;
 import java.util.Date;
 
+@Data
 public class EntityCriteria implements Serializable {
     private static final long serialVersionUID = -1846402828650498375L;
 
@@ -10,15 +15,23 @@ public class EntityCriteria implements Serializable {
     private Date createAt;
     private DateRange updateRange;
     private DateRange createRange;
+
+    /**
+     * order by 字句，将直接拼接到 sql 后面<br>
+     * 赋值样例： user_name, login_time desc, update_at asc <br>
+     * 注意：字段名称使用库表的列（column）的名称
+     */
     private String orderBy;
 
     /**
      * offset 是分页查询用的
      */
+    @Setter(AccessLevel.NONE)
     private Integer offset;
     /**
      * limit 是分页查询用的
      */
+    @Setter(AccessLevel.NONE)
     private Integer limit;
 
     /**
@@ -30,56 +43,6 @@ public class EntityCriteria implements Serializable {
         limit = pageSize > 0 ? pageSize : 20;
         int offset = (pageNo - 1) * pageSize;
         this.offset = offset > 0 ? offset : 0;
-    }
-
-    public Date getUpdateAt() {
-        return updateAt;
-    }
-
-    public void setUpdateAt(Date updateAt) {
-        this.updateAt = updateAt;
-    }
-
-    public Date getCreateAt() {
-        return createAt;
-    }
-
-    public void setCreateAt(Date createAt) {
-        this.createAt = createAt;
-    }
-
-    public DateRange getUpdateRange() {
-        return updateRange;
-    }
-
-    public void setUpdateRange(DateRange updateRange) {
-        this.updateRange = updateRange;
-    }
-
-    public DateRange getCreateRange() {
-        return createRange;
-    }
-
-    public void setCreateRange(DateRange createRange) {
-        this.createRange = createRange;
-    }
-
-    /**
-     * order by 字句，将直接拼接到 sql 后面<br>
-     * 赋值样例： user_name, login_time desc, update_at asc <br>
-     * 注意：字段名称使用库表的列（column）的名称
-     */
-    public String getOrderBy() {
-        return orderBy;
-    }
-
-    /**
-     * order by 字句，将直接拼接到 sql 后面<br>
-     * 赋值样例： user_name, login_time desc, update_at asc <br>
-     * 注意：字段名称使用库表的列（column）的名称
-     */
-    public void setOrderBy(String orderBy) {
-        this.orderBy = orderBy;
     }
 
     public Integer getOffset() {
